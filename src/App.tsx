2ec1adfee3ef11c9d6a6fb8c9be38246c3c93675
Fedeya <elfedeminaya@gmail.com>
2020-06-26 15:02:08 -0300

    [Feat] used context and added styles for news

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import Search from './components/Search';
+import NewsList from './components/NewsList';
+
+import NewsProvider from './context/NewsContext';
 
 function App() {
   return (
-    <div className="container">
-      <h1>REACT NEWS</h1>
-      <Search />
-    </div>
+    <NewsProvider>
+      <div className="container">
+        <h1>REACT NEWS</h1>
+        <Search />
+        <NewsList />
+      </div>
+    </NewsProvider>
   )
 }
 
